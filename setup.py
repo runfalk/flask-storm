b09bb4925795572035b85fcb56e6309c3993187b
Colin Watson <cjwatson@canonical.com>
2019-12-03 22:03:46 +0000

    Drop Python 3.3 support
    Tests no longer work for various reasons (Travis no longer supports it,
    and Sphinx fails to install on 3.3 in tox), so given that 3.3 went EOL
    in September 2017 it seems simplest to drop support for it.

--- setup.py
+++ setup.py
@@ -139,7 +139,6 @@ if __name__ == "__main__":
             "Intended Audience :: Developers",
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.3",
             "Programming Language :: Python :: 3.4",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
